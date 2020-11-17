7167f052a65306cdf27bd7eb4257b207c21d1006
farahatqah <55783293+farahatqah@users.noreply.github.com>
2020-11-18 01:45:23 +0800

    Add files via upload

---
+++ parcel_pickup.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+  <?php
+    echo "<br>Parcel has succesfully picked up!<br><br>";
+   ?>
+   <a href="parcel_system.html">Homepage</a>
+   </body>
+ </html>
---
+++ warning.php
@@ -0,0 +1,8 @@
+<?php
+function function_alert($message) {
+    // Display the alert box
+    echo "<script>alert('$message');</script>";
+}
+// Function call
+function_alert("Message sent to recipient: Recipient must pick up within 2 days");
+ ?>
